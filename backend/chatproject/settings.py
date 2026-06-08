62c89ae12d853b3984bd53d0f08d85fce1e55b94
ajayhkr20 <ajayhkr20@email.com>
2026-06-08 15:31:11 +0530

    Update settings

--- backend/chatproject/settings.py
+++ backend/chatproject/settings.py
@@ -51,11 +51,12 @@ TEMPLATES = [{
 ASGI_APPLICATION = 'chatproject.asgi.application'
 WSGI_APPLICATION = 'chatproject.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL")
+    )
 }
 
 # In-memory channel layer (switch to Redis for production)
